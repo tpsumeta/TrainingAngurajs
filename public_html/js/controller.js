6e03f875e84dd4efa421a5bc66f06a1efe457fb0
tpsumeta <tpsumeta@gmail.com>
2015-03-22 15:49:00 +0700

    search filter

--- public_html/js/controller.js
+++ public_html/js/controller.js
@@ -1,22 +1,43 @@
-App.controller('myControler', function ($scope) {
- // $scope.name = "Sumeta";
-    $scope.btnName = function (){
-        alert('Hello '+$scope.name);
+App.controller('myControler', function ($scope,$rootScope) {
+    // $scope.name = "Sumeta";
+    $scope.btnName = function () {
+        alert('Hello ' + $scope.name);
     };
     // create person object
     $scope.person = {};
+    $rootScope.person = $scope.person;
 });
 
-App.controller('indexController',function ($scope){
+App.controller('indexController', function ($scope) {
     $scope.page = 'index';
 });
 
-App.controller('bookController',function ($scope,$http){
+App.controller('bookController', function ($scope, $http) {
+    $http.get('http://localhost:8383/TranAngurajs/mockup/book.json')
+            .success(function (data) {
+                $scope.books = data.books;
+            })
+            .error(function (status) {
+                alert(status);
+            });
+
+    $scope.page = 'book';
+//    $http.post('http://localhost:8383/TranAngurajs/mockup/book.json', $scope.page)
+//            .success(function (data) {
+//                $scope.books = data.books;
+//            })
+//            .error(function (status) {
+//                //alert(status);
+//            });
+
     
-        $scope.page = 'book';
-        $scope.books = [
-            {name:'abc',price:22},
-            {name:'ฮฮฮ',price:0},
-            {name:'java',price:34},
-             ];
+
+
+
+//    $scope.books = [
+//        {name: 'abc', price: 22},
+//        {name: 'ฮฮฮ', price: 0},
+//        {name: 'java', price: 34},
+//    ];
+
 });
